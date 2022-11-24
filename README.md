3c922a6b603fc2c940105c67167d398f276c8f19
VincedeWit <117919319+VincedeWit@users.noreply.github.com>
2022-11-24 14:34:55 +0000

    clarified delete/deploy-commands for github

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 (DND) Discord bot 
 
+To create commands use "depoy-commands.js" in the shell
+
+To delete commands use "delete-command.js" in the shell
+
--- delete-command.js
+++ delete-command.js
@@ -4,9 +4,8 @@ const token = process.env['TOKEN']
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-// ...
 
-// // for guild-based commands
+// for server commands
 // rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
 // 	.then(() => console.log('Successfully deleted guild command'))
 // 	.catch(console.error);
