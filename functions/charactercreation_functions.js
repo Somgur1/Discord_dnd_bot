919576b07ac85d3fac47d149fab3a6b880de9b65
Vince <71010397+Somgur1@users.noreply.github.com>
2023-01-21 08:39:15 +0000

    Now also displays what you rolled in "official rolls" In "official rolls" rerolls the 1, as per rules.

--- functions/charactercreation_functions.js
+++ functions/charactercreation_functions.js
@@ -12,29 +12,45 @@ const races = ["Human", "Elf", "Dwarf", "Halfling", "Dragonborn", "Gnome", "Half
 const classes = ["Fighter", "Cleric", "Wizard", "Rogue", "Barbarian", "Bard", "Druid", "Monk", "Paladin",
 "Ranger", "Sorcerer", "Warlock", "Wizard", "Artificer", "Blood Hunter"];
 
-function rollForStat(){
+function rollForStat(interaction){
   // Create an array to store the dice rolls
   const rolls = [];
-
+  let sum
+  let roll
+if (interaction.options.getSubcommand() === 'official_random_abilities'){
   // Roll the dice 4 times
   for (let i = 0; i < 4; i++) {
-    // Generate a random number between 1 and 6
-    const roll = Math.floor(Math.random() * 6) + 1;
-    // Add the roll to the array
-    rolls.push(roll);
-  }
-
-  // Sort the rolls in ascending order
+     roll = Math.floor(Math.random() * 6) + 1;
+    while(roll === 1) {
+        roll = Math.floor(Math.random() * 6) + 1;
+    }
+    rolls.push(roll);  
+}
+   // Sort the rolls in ascending order
   rolls.sort((a, b) => a - b);
 
   // Drop the lowest roll
   rolls.shift();
 
   // Add the remaining rolls together
-  const sum = rolls.reduce((total, roll) => total + roll, 0);
+  sum = rolls.reduce((total, roll) => total + roll, 0);
+}
+  else{
+    sum = Math.floor(Math.random() * 20 + 1)
+  }
+  
 
+  sumRolls = String(rolls);
+  sumRolls = sumRolls.replaceAll(",", " + ");
+  sumRolls = "(" + sumRolls + ")";
+
+  rollsarray = {
+    allrolls: sumRolls,
+    total: sum
+  }
+  console.log(rollsarray)
   // Return the sum
-  return sum
+  return rollsarray
 }
 
 function generateRandomCharacter(interaction) {
@@ -46,31 +62,33 @@ function generateRandomCharacter(interaction) {
     let intelligence;
     let wisdom;
     let charisma;
-  if (interaction.options.getSubcommand() === 'complete_random_abilites'){
-    console.log("complete random")
-     race = races[Math.floor(Math.random() * races.length)];
-     characterClass = classes[Math.floor(Math.random() * classes.length)];
-     strength = Math.floor(Math.random() * 20 + 1);
-     dexterity = Math.floor(Math.random() * 20 + 1);
-     constitution = Math.floor(Math.random() * 20 + 1);
-     intelligence = Math.floor(Math.random() * 20 + 1);
-     wisdom = Math.floor(Math.random() * 20 + 1);
-     charisma = Math.floor(Math.random() * 20 + 1);
-  }
-  else{
-    console.log("official random")
-    // Generate random values for the character's attributes
-     race = races[Math.floor(Math.random() * races.length)];
-     characterClass = classes[Math.floor(Math.random() * classes.length)];
-     strength = rollForStat();
-     dexterity = rollForStat();
-     constitution = rollForStat();
-     intelligence = rollForStat();
-     wisdom = rollForStat();
-     charisma = rollForStat();
-  }
   
-    
+     race = races[Math.floor(Math.random() * races.length)];
+     characterClass = classes[Math.floor(Math.random() * classes.length)];
+     strength = rollForStat(interaction);
+     dexterity = rollForStat(interaction);
+     constitution = rollForStat(interaction);
+     intelligence = rollForStat(interaction);
+     wisdom = rollForStat(interaction);
+     charisma = rollForStat(interaction);
+  //}
+  
+    if (interaction.options.getSubcommand() === 'official_random_abilities'){
+      strength = strength.allrolls + "\n" + strength.total,
+            dexterity = dexterity.allrolls + "\n" + dexterity.total,
+            constitution = constitution.allrolls + "\n" + constitution.total,
+            intelligence = intelligence.allrolls + "\n" + intelligence.total,
+            wisdom = wisdom.allrolls + "\n" + wisdom.total,
+            charisma = charisma.allrolls + "\n" + charisma.total
+    }
+  else{
+    strength = strength.total;
+    dexterity = dexterity.total;
+    constitution = constitution.total;
+    intelligence = intelligence.total;
+    wisdom = wisdom.total;
+    charisma = charisma.total;
+  }
 
     // Create a new character object with the random values
     const character = {
