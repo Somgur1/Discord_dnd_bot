1fceb4d8fb31bab26a55181fd02be7e24e998e3c
Vince <71010397+Somgur1@users.noreply.github.com>
2023-12-19 10:49:09 +0100

    Add files via upload

--- functions/roll.js
+++ functions/roll.js
@@ -1,58 +1,44 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const {
   EmbedBuilder,
 } = require('discord.js');
 
 module.exports = {
-  roll_dice: function(message_split, msg){
-    dice_roll = null;
-    userCommand = message_split[1];
-    dice_roll = message_split[1];
-    if (dice_roll == null){
-      return msg.reply("Please enter a dice to roll");
-    }
-    var url = 'https://rolz.org/api/?' + dice_roll + '.json';
-    fetch(url)
-        .then(response => {
-          return response.json();
-        })
-        .then(user => {
-          var result = String(user["result"]);
-          const diceEmbed = new EmbedBuilder()
-              .setColor(0x0099FF)
-              .setTitle('Dice roller')
-              .addFields(
-                  { name: 'input', value: user["input"] },
-                  { name: 'result', value: result, inline: true },
-                  { name: 'details', value: user["details"], inline: true }
-              );
-          msg.reply({ embeds: [diceEmbed] });
-        })
-        .catch((error) => {
-          msg.reply("Invalid input");
-        });
-  },
-  roll_dice_slash_command: function(interaction){
+  roll_dice: function(interaction) {
     const dice = interaction.options.getString('dice');
+    const GM = interaction.options.getUser('gm-roll');
+    const server_id = interaction.guildId;
+    const server_name = interaction.client.guilds.cache.get(server_id);
+    const user_tree = interaction.user;
+    const user_id = interaction.user.id;
+    const username = interaction.user.username;
     const url = 'https://rolz.org/api/?' + dice + '.json';
     fetch(url)
-        .then(response => {
-          return response.json();
-        })
-        .then(user => {
-          var result = String(user["result"]);
-          const diceEmbed = new EmbedBuilder()
-              .setColor('Red')
-              .setTitle('Dice roller')
-              .addFields(
-                  { name: 'input', value: user["input"] },
-                  { name: 'result', value: result, inline: true },
-                  { name: 'details', value: user["details"], inline: true }
-              );
+      .then(response => {
+        return response.json();
+      })
+      .then(user => {
+        var result = String(user["result"]);
+        const diceEmbed = new EmbedBuilder()
+          .setColor('Red')
+          .setTitle('Dice roller')
+          .addFields(
+            { name: 'input', value: user["input"] },
+            { name: 'result', value: result, inline: true },
+            { name: 'details', value: user["details"], inline: true }
+          );
+        if (GM) {
+          GM.send({ content: `<@${user_id}> rolled a ${dice} in ${server_name} (<#${interaction.channel.id}>)`, embeds: [diceEmbed] });
+
+          interaction.reply({ embeds: [diceEmbed], ephemeral: true });
+        }
+        else {
           interaction.reply({ embeds: [diceEmbed] });
-        })
-        .catch((error) => {
-        });
+        }
+
+      })
+      .catch((error) => {
+      });
   }
 }
 
