c33a8785b10a7dcbac7dfaf772299eae7b4edead
Vince <71010397+Somgur1@users.noreply.github.com>
2023-12-19 10:49:44 +0100

    Add files via upload

--- commands/roll.js
+++ commands/roll.js
@@ -1,15 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 const roll_dice = require('./../functions/roll');
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('roll_dice')
-		.setDescription('Roll a dice')
+  data: new SlashCommandBuilder()
+    .setName('roll_dice')
+    .setDescription('Roll a dice')
     .addStringOption(option =>
-		  option.setName('dice')
-			.setDescription('What dice to role. (1d20)')
-			.setRequired(true)
-                   ),
-	async execute(interaction) {
-		roll_dice.roll_dice_slash_command(interaction);
-	},
+      option.setName('dice')
+        .setDescription('What dice to role. (1d20)')
+        .setRequired(true)
+    )
+    .addUserOption(option =>
+      option
+        .setName('gm-roll')
+        .setDescription('Who is the GM')),
+  async execute(interaction) {
+    roll_dice.roll_dice(interaction);
+  },
 };
